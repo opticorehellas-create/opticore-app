7511e76724654e66beb0f572aa7299b5b7e025a0
opticorehellas-create <opticorehellas@gmail.com>
2026-05-08 13:17:19 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'opticore-v1';
+const urlsToCache = [
+  '/opticore-app/',
+  '/opticore-app/index.html',
+  '/opticore-app/manifest.json'
+];
+
+// Εγκατάσταση
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch - offline first
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('/opticore-app/index.html');
+    })
+  );
+});
+
+// Ενημέρωση cache
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
